d35508a72b5366c459165daaadc62a34ed7d698f
Ornic03 <gianellamlazarini2@gmail.com>
2026-06-15 15:20:57 -0300

    NetBeans

---
+++ programacion2NetBeans/src/main/java/com/mycompany/programacion2netbeans/Programacion2NetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacion2netbeans;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Programacion2NetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
